556370364f31ab0ba703389b1bc283c12fc6417d
Gustavo Leste <gustavoleste@protonmail.com>
2019-02-20 12:27:18 -0300

    Home screen

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Home from './src/screens/Home'
+
+const App = () =>(
+	<Home />
+)
+
+export default App
---
+++ src/screens/Home/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Container, Header, Tab, Tabs, Text,Title,Icon,TabHeading, Body} from 'native-base'
+
+const Home = () => (
+        <Container>
+                <Header  hasTabs noLeft>
+                        <Body>
+                                <Title>React Jobs</Title>
+                        </Body>                        
+                </Header>
+                <Tabs>
+                        <Tab heading={<TabHeading><Icon name='work' type='MaterialIcons'/><Text>VAGAS</Text></TabHeading>}>
+                                
+                        </Tab>                       
+                        <Tab heading={<TabHeading><Icon  name='ios-bookmark' type='Ionicons' /><Text>SALVOS (0)</Text></TabHeading>}>
+                              
+                        </Tab>
+                </Tabs>
+        </Container>
+)
+
+export default Home
